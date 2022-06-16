66d3617c78451da9fb8eb5931459dd3e9c26cbe5
SahilMamedov <sahilim@code.edu.az>
2022-06-16 17:15:51 +0400

    Feat: Datnet

---
+++ FirstApp/FirstApp/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+
+            return View();
+            //ViewResult view = new ViewResult();
+            //view.ViewName = "index";
+            //return Json(new
+            //{
+            //    name = "sahil",
+            //    age = 25
+
+            //});
+            //return Content($"index action ishledi -){id}");
+        }
+        public IActionResult About()
+        {
+            return View();
+            //return Content("About action ishledi --)");
+        }
+    }
+}
